76b2c8885302f515f0b40d37b4f7de8a75b2f9a4
Bing Zhou <sarlanori@qq.com>
2017-07-05 11:17:41 +0800

    适配最新网易云音乐

--- wyMusicDownloader.py
+++ wyMusicDownloader.py
@@ -1,9 +1,8 @@
 #! coding:utf-8
 
 # Filename: wyMusicDownloader.py
-# Author: sarlanori
+# Author: bing zhou
 # Created Date: 2015-02-21
-# Modified Date: 2015-02-25
 
 # pylint: disable=C0111,C0301,C0103,W0512
 
@@ -15,6 +14,7 @@ import json
 import sys
 import os.path
 import getopt
+import codecs
 
 import encryptutil
 
@@ -37,8 +37,8 @@ def get_song_list_ids(page_source):
 
 
 def get_song_list_name(page_source):
-    match = re.search(r'<h2 class=\"f-ff2 f-brk\">(.*)</h2>', page_source)
-    return match.group(1).decode('utf-8')
+    match = re.search(r'<h2 class=\"f-ff2( f-brk)?\">(.*)</h2>', page_source)
+    return match.group(2).decode('utf-8')
 
 
 def get_song_detail_list(song_id_list, bitrate):
@@ -65,7 +65,7 @@ def get_mp3_url_list(song_detail_list, bitrate):
         song_id_list.append(detail['id'])
     data = {
         'ids': song_id_list,
-        # br 代表比特率(Bitrate)，常用取值：192000、128000、320000、990000...值越大代表音质越好，文件也越大
+        # br 代表比特率(Bitrate)，常用取值：128000、192000、320000、990000...值越大代表音质越好，文件也越大
         'br': bitrate
     }
     data = json.dumps(data)
@@ -78,43 +78,55 @@ def get_mp3_url_list(song_detail_list, bitrate):
         detail['bitrate'] = item['br']
     return song_detail_list
 
+def calc_total_file_size(song_detail_list):
+    total_size = 0
+    for detail in song_detail_list:
+        total_size += detail['size']
+    return total_size
+
 def start(song_list_url, bitrate, local_save_path):
-    print '正在下载歌单源码...'.decode('utf-8')
+    print u'正在下载歌单源码...'
     page_source = get_song_list_page_source(song_list_url)
-    print '正在获取歌单名称...'.decode('utf-8')
+    print u'正在获取歌单名称...'
     song_list_name = get_song_list_name(page_source)
 
     if local_save_path is None:
         local_save_path = song_list_name
 
-    song_detail_list = None
-
-    detail_list_save_path = os.path.join(local_save_path, song_list_name + ".json")
-    if os.path.exists(detail_list_save_path):
-        song_detail_list = json.loads(detail_list_save_path)
-    else:
-        print '正在获取歌曲 ids...'.decode('utf-8')
-        song_id_list = get_song_list_ids(page_source)
-        print '正在获取歌曲详细信息...'.decode('utf-8')
-        song_detail_list = get_song_detail_list(song_id_list, bitrate)
-
     if os.path.exists(local_save_path) is False:
         os.mkdir(local_save_path)
 
-    i = 1
+    song_detail_list = None
+
+    song_detail_list_save_path = os.path.join(local_save_path, 'song-list-%dkbps.json' % (bitrate / 1000))
+    if os.path.exists(song_detail_list_save_path):
+        with codecs.open(song_detail_list_save_path, 'r') as handle:
+            song_detail_list = json.load(handle)
+    else:
+        print u'正在获取歌曲 ids...'
+        song_id_list = get_song_list_ids(page_source)
+        print u'正在获取歌曲详细信息...'
+        song_detail_list = get_song_detail_list(song_id_list, bitrate)
+
+        with codecs.open(song_detail_list_save_path, "w") as handle:
+            json.dump(song_detail_list, handle, ensure_ascii=False)
+
+    total_file_size = calc_total_file_size(song_detail_list)
+    download_size = 0
     for song_detail in song_detail_list:
         file_path = os.path.join(local_save_path, song_detail['name'].replace("/", "\\") + '-' + song_detail['artist'] + '.mp3')
-        print '正在下载(%d/%d) %s' % (i, len(song_detail_list), song_detail['name'])
-        i += 1
+        print u'正在下载(%d/%d) %s...剩余文件大小：%f MB' % (song_detail_list.index(song_detail) + 1, \
+            len(song_detail_list), song_detail['name'], (total_file_size - download_size) / 1024.0**2)
+        download_size += song_detail['size']
         if os.path.exists(file_path):
-            continue
+            if os.path.getsize(file_path) == song_detail['size']:
+                continue
+            else:
+                os.remove(file_path)
         urllib.urlretrieve(song_detail['mp3url'], file_path)
 
-    with open(detail_list_save_path, "w") as handle:
-        handle.write(json.dumps(song_detail_list))
-
 def usage():
-    print '使用方法：python wyMusicDownloader.py [-o local/save/path](可选) [-b bitrate](可选) [song list url]'.decode('utf-8')
+    print u'使用方法：python wyMusicDownloader.py [-o local/save/path](可选) [-b bitrate](可选) [song list url]'
 
 if __name__ == '__main__':
     # sys.argv 第一个参数为执行脚本的名称
@@ -139,8 +151,8 @@ if __name__ == '__main__':
         elif o in ('-o', '--output'):
             output = a
         elif o in ('-b', '--bitrate'):
-            _bitrate = a
+            _bitrate = int(a)
 
     start(_song_list_url, _bitrate, output)
 
-    print '下载完成！'
+    print u'下载完成！'
